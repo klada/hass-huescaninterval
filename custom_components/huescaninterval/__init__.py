5f82d92dc151a428f5cce6e030aed06854a4c3bc
Daniel Klaffenbach <danielklaffenbach@gmail.com>
2020-04-15 20:58:59 +0200

    Remove remote-related code from platform setup
    HomeAssistant has native support for Hue events we do not need remote
    entities any more.

--- custom_components/huescaninterval/__init__.py
+++ custom_components/huescaninterval/__init__.py
@@ -1,71 +1,32 @@
-"""Hue remotes."""
+from datetime import timedelta
 import logging
-
+from homeassistant.components.hue import DOMAIN as HUE_DOMAIN
+from homeassistant.helpers.event import async_track_time_interval
 from homeassistant.const import CONF_SCAN_INTERVAL
-from homeassistant.components.remote import (  # noqa: F401
-    PLATFORM_SCHEMA,
-    RemoteDevice,
-)
 
-from . import DOMAIN
-from .data_manager import (
-    DEFAULT_SCAN_INTERVAL,
-    HueSensorBaseDevice,
-    HueSensorData,
-)
-from .hue_api_response import REMOTE_MODELS
-
+DOMAIN = "huescaninterval"
+DEFAULT_SCAN_INTERVAL = timedelta(seconds=0.5)
 _LOGGER = logging.getLogger(__name__)
 
-REMOTE_ICONS = {
-    "RWL": "mdi:remote",
-    "ROM": "mdi:remote",
-    "ZGP": "mdi:remote",
-    "FOH": "mdi:light-switch",
-    "Z3-": "mdi:light-switch",
-}
 
+async def async_setup(hass, config):
+    """Set up remote platform by registering it on each Hue Bridge."""
+    for bridge_entry_id, bridge in hass.data[HUE_DOMAIN].items():
+        # Set up updates at scan_interval
+        async def _update_remotes(now=None):
+            """Request a bridge data refresh so remote states are updated."""
+            await bridge.sensor_manager.coordinator.async_refresh()
+            _LOGGER.debug("Update requested at %s", now)
 
-async def async_setup_platform(
-    hass, config, async_add_entities, discovery_info=None
-):
-    """Initialise Hue Bridge connection."""
-    if DOMAIN not in hass.data:
-        hass.data[DOMAIN] = HueSensorData(hass)
-
-    await hass.data[DOMAIN].async_add_platform_entities(
-        HueRemote,
-        REMOTE_MODELS,
-        async_add_entities,
-        config.get(CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL),
-    )
-
-
-class HueRemote(HueSensorBaseDevice, RemoteDevice):
-    """Class to hold Hue Remote basic info."""
-
-    @property
-    def state(self):
-        """Return the state of the remote."""
-        return self.sensor_data["state"]
-
-    @property
-    def icon(self):
-        """Icon to use in the frontend, if any."""
-        data = self.sensor_data
-        if data:
-            icon = REMOTE_ICONS.get(data["model"])
-            if icon:
-                return icon
-        return "mdi:remote"  # pragma: no cover
-
-    @property
-    def force_update(self):
-        """Force update."""
-        return True
-
-    def turn_on(self, **kwargs):
-        """Do nothing."""
-
-    def turn_off(self, **kwargs):
-        """Do nothing."""
+        remote_sc = max(
+            DEFAULT_SCAN_INTERVAL, config.get(CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL)
+        )
+        if remote_sc < bridge.sensor_manager.coordinator.update_interval:
+            # Add listener to update remotes at high rate
+            # TODO change sm.coordinator.update_interval instead.
+            #  It can be done right here, but any bridge.reset event
+            #  would re-write it, so we maintain our own scheduler meanwhile
+            async_track_time_interval(
+                hass, _update_remotes, remote_sc,
+            )
+    return True
